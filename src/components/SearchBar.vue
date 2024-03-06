db2e0f6e7f4c2366029dedacb91708df883cb98d
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-03-06 14:56:50 +0100

    search bar stylization

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -13,7 +13,7 @@ export default {
 </script>
 
 <template>
-    <nav>
+    <nav id="navBar">
         <div class="logo">
             boolflix
         </div>
@@ -27,5 +27,43 @@ export default {
 </template>
 
 <style lang="scss">
+@use '../styles/variables' as *;
+
+#navBar {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 20px 30px;
+    position: fixed;
+
+    width: 100%;
+    height: 70px;
+
+    background-color: black;
+
+    .logo {
+        color: $titleAndBtn;
+        font-size: 1.5em;
+        text-transform: uppercase;
+    }
+    .search {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+
+        #searchBar {
+            height: 24px;
+            padding: 4px;
+        }
+        .searchBtn {
+            padding: 4px 8px;
+            background-color: $titleAndBtn;
+            border: 1px solid $titleAndBtn;
+            border-radius: 6px;
+        }
+    }
+
+
+}
 
 </style>

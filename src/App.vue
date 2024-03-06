4fc45572d3f9ffe03d41a49bbc846776c0c02013
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-03-06 14:02:49 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+// import axios
+import axios from 'axios';
+// store
+import { store } from './store.js';
+
+export default {
+  components: {
+
+  },
+
+  data() {
+    return {
+      store,
+
+      apiKey: '',
+    }
+  },
+  
+  methods: {
+    
+  },
+
+  created() {
+    
+  },
+}
+</script>
+
+<template>
+</template>
+
+<style>
+</style>

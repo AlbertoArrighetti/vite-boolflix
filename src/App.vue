c7854ec1d9e8b90dee3aabd77d522e2859576993
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-03-06 14:17:27 +0100

    API call and key (to be added)

--- src/App.vue
+++ src/App.vue
@@ -22,13 +22,30 @@ export default {
   },
 
   created() {
+
+    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=Ritorno al futuro`)
+    .then((res) => {
+      console.log(res.data.results)
+    });
     
   },
 }
 </script>
 
 <template>
+
+<div>
+  <h1>ciao</h1>
+</div>
+
 </template>
 
-<style>
+<style lang="scss">
+
+div {
+
+  h1 {
+    color: red;
+  }
+}
 </style>
